4085a7ead1c913fa0c5c011fb51a1d0a6105a84e
Sean Herbert <seanh@email.arizona.edu>
2019-09-08 20:48:02 -0700

    trimmed down the DRV8825 class

---
+++ Stepper_Motor_HAT_code/python/DRV8825_scratch.py
@@ -0,0 +1,55 @@
+import RPi.GPIO as GPIO
+import time
+
+MotorDir = [
+    'forward',
+    'backward',
+]
+
+
+
+class DRV8825():
+    def __init__(self, dir_pin, step_pin, enable_pin, mode_pins):
+        self.dir_pin = dir_pin
+        self.step_pin = step_pin        
+        self.enable_pin = enable_pin
+       
+        
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(self.dir_pin, GPIO.OUT)
+        GPIO.setup(self.step_pin, GPIO.OUT)
+        GPIO.setup(self.enable_pin, GPIO.OUT)
+        
+    def digital_write(self, pin, value):
+        GPIO.output(pin, value)
+        
+    def Stop(self):
+        self.digital_write(self.enable_pin, 1)
+    
+    
+        
+    def TurnStep(self, Dir, steps, stepdelay=0.005):
+        if (Dir == MotorDir[0]):
+            print ("forward")
+            self.digital_write(self.enable_pin, 0)
+            self.digital_write(self.dir_pin, 0)
+        elif (Dir == MotorDir[1]):
+            print ("backward")
+            self.digital_write(self.enable_pin, 0)
+            self.digital_write(self.dir_pin, 1)
+        else:
+            print ("the dir must be : 'forward' or 'backward'")
+            self.digital_write(self.enable_pin, 1)
+            return
+
+        if (steps == 0):
+            return
+            
+        print ("turn step:",steps)
+        for i in range(steps):
+            self.digital_write(self.step_pin, True)
+            time.sleep(stepdelay)
+            self.digital_write(self.step_pin, False)
+            time.sleep(stepdelay)
+
