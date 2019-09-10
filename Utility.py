29562508eb5bfda501dc819eb6bc96ab03425112
Sean Herbert <seanh@email.arizona.edu>
2019-09-09 23:06:08 -0700

    added empty System class and added Utility class with setPathology and minPathology and maxPathology

---
+++ Utility.py
@@ -0,0 +1,10 @@
+
+class Utility:
+    def __init__(self):
+        self.minPathology = 0
+        self.maxPathology = 0
+    def setPathology(self, min, max):
+        self.minPathology = min
+        self.maxPathology = max
+    
+    
