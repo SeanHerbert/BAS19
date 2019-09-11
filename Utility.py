6628e844c47e96c9b94c2bd714abc45502dcdf17
Sean Herbert <seanh@email.arrizona.edu>
2019-09-10 20:59:47 -0700

    added dataEnter method to open excel file

--- Utility.py
+++ Utility.py
@@ -1,4 +1,9 @@
 
+from openpyxl import Workbook
+from openpyxl import load_workbook
+import os
+
+
 class Utility:
     def __init__(self):
         self.minPathology = 0
@@ -6,5 +11,17 @@ class Utility:
     def setPathology(self, min, max):
         self.minPathology = min
         self.maxPathology = max
+    def dataEnter(self):
+        self.wb = Workbook()
+        self.ws = self.wb.active
+        self.ws.title = 'newFile'
+        self.filename = 'newFile.xlsx'
+        self.df = 'DataFiles\\'
+        self.path = self.df + self.filename
+        self.wb.save(self.path)
+        os.startfile(self.path)
+
+
+
     
     
