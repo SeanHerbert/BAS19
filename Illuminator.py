df778d1f48b8bfbe28d5496ff5425e40c0ce5a38
Sean Herbert <seanh@email.arizona.edu>
2019-09-14 00:08:14 -0700

    added Illuminator class and its driver

---
+++ Illuminator.py
@@ -0,0 +1,54 @@
+import RPi.GPIO as io
+io.setwarnings(False)
+
+class Illuminator(): # Define our class
+    def __init__(self,data,clock,bright):
+        self.setBrightness(bright)
+        self.da = data
+        self.ck = clock
+        
+        io.setmode(io.BCM)
+        io.setup(self.ck,io.OUT)
+        io.setup(self.da,io.OUT)
+        io.output(self.ck, io.HIGH)
+        io.output(self.da, io.HIGH)
+        self.led = self.br<<24 
+     
+    def setBrightness(self,brightness):
+        if brightness > 31:
+            brightness = 31
+        if brightness < 0:
+            brightness = 0
+        self.br = brightness | 0xE0
+
+    def on(self,col):
+        
+            self.led = (self.br<<24)|(col[2]<<16)|(col[1]<<8)|col[0]
+            self.show()
+     
+    
+ 
+    def show(self):
+        io.output(self.da,io.LOW)
+        for i in range(0,32): # send header
+            io.output(self.ck, io.LOW)
+            io.output(self.ck, io.HIGH)
+       
+            d = self.led # send data
+            for j in range(0,32):
+                io.output(self.ck, io.LOW)
+                if d & 0x80000000 :
+                    io.output(self.da, io.HIGH)
+                else:
+                    io.output(self.da, io.LOW)
+                d = d << 1
+                io.output(self.ck, io.HIGH)
+        io.output(self.da, io.LOW)
+        for i in range(0,33): # send footer
+            io.output(self.ck, io.LOW)
+            io.output(self.ck, io.HIGH)
+    def off(self):
+        self.led = (self.br<<24)|(0<<16)|(0<<8)|0
+        self.show()
+        
+
---
+++ Illuminator_test.py
@@ -0,0 +1,32 @@
+import RPi.GPIO as io
+io.setwarnings(False)
+
+import time
+import random
+from Illuminator import Illuminator
+
+brightness = 31 # (0-31)
+dataPin = 14 ; clockPin = 15 # GPIO pin names
+red = 0; green = 255; blue = 0; # values between 0-255
+switch = 1
+
+led = Illuminator(dataPin,clockPin,brightness)
+
+i=0
+while(1):
+    green =(green +222) %256
+    blue = (blue+100)%256
+    red = (red+ 154)%256
+    if(i%2 ==1):
+        led.setBrightness(31)
+    else:
+        led.setBrightness(1)
+    led.on((green,blue,red)) 
+    
+    time.sleep(1)
+    led.off()
+    
+    time.sleep(1)
+    i+=1
+     
+
