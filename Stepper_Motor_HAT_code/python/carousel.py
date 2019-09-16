f1af4583bb5b31a3ca0202ccf8f114a22db1eed4
Sean Herbert <seanh@email.arizona.edu>
2019-09-15 21:16:31 -0700

    factored in gear ratio to carousel class

--- Stepper_Motor_HAT_code/python/carousel.py
+++ Stepper_Motor_HAT_code/python/carousel.py
@@ -8,41 +8,38 @@ class carousel:
         self.motor = DRV8825(dir_pin=13, step_pin=19, enable_pin=12, mode_pins=(16, 17, 20))
         self.steps = 0
         self.curPos = 1
-    
+         
     def stepForward(self):
-        self.motor.TurnStep(Dir='forward', steps=32, stepdelay = 0.000001)
-        self.steps += 32
-        self.steps = self.steps%6400
+        self.motor.TurnStep(Dir='forward', steps=192, stepdelay = 0.000001)
+        self.steps += 192
+        self.steps = self.steps% 38400
     def stepBackward(self):
-        self.motor.TurnStep(Dir='backward', steps=32, stepdelay = 0.000001)
-        self.steps -= 32
-        self.steps = self.steps%6400
+        self.motor.TurnStep(Dir='backward', steps=192, stepdelay = 0.000001)
+        self.steps -= 192
+        self.steps = self.steps%38400
     def nextSlide(self):
-        self.motor.TurnStep(Dir='forward', steps=320, stepdelay = 0.000001)
-        self.steps += 320
+        self.motor.TurnStep(Dir='forward', steps=1920, stepdelay = 0.000001)
+        self.steps += 1920
         self.curPos+=1
         self.curPos = (self.curPos % 20) +1
-        self.steps = self.steps%6400
+        self.steps = self.steps%38400
     def prevSlide(self):
-        self.motor.TurnStep(Dir='backward', steps=320, stepdelay = 0.000001)
-        self.steps -= 320
+        self.motor.TurnStep(Dir='backward', steps=1920, stepdelay = 0.000001)
+        self.steps -= 1920
         self.curPos-=1
         self.curPos = (self.curPos % 20) + 1
-        self.steps = self.steps%6400
+        self.steps = self.steps%38400
     def zeroPos(self):
         self.curPos = 1
         self.steps = 0
     def moveToSlide(self, next):
-        print(self.curPos)
         if(self.curPos<next and (next-self.curPos)<11):
             turns = (next-self.curPos)
-            print(turns)
             for x in range(0,turns):
                 self.nextSlide()
             self.curPos = next
         elif(self.curPos<next and (next-self.curPos)>=11):
             turns = (self.curPos -next)%20
-            print(turns)
             for x in range(0,turns):
                 self.prevSlide()
             self.curPos = next
