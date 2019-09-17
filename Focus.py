f773be539827022be642801de8e28871511b8e25
Sean Herbert <seanh@email.arrizona.edu>
2019-09-17 13:15:54 -0700

    addedd autofocus routine from SDD flowchart

--- Focus.py
+++ Focus.py
@@ -8,10 +8,17 @@ from imutils import paths
 class Focus():
     def __init__(self):
         self.motor = DRV8825(dir_pin=13, step_pin=19, enable_pin=12, mode_pins=(16, 17, 20))
-        
+        self.curPos = self.goToMax()
+        self.maxPos = 35
+        self.minPos = 25
     
     def varianceofLap(image):
         return cv2.Laplacian(image,cv2.CV_64F).var()
+
+    def goToMax(self):
+        #moveToMax pos
+        max = 35
+        return max
 
     def vidOn(self):
         print("vidOn")
@@ -23,9 +30,49 @@ class Focus():
         
     
     def autoFocus(self):
-        print("autoFocus")
-    
-    
+        # for imagePaths in paths.list_images('images'):
+        #     image = cv2.imread(imagePaths)
+        #     imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        loop = 0
+        lt = 0
+        i=0
+
+        for imagePaths in paths.list_images('images'):
+            image = cv2.imread(imagePaths)
+            imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            v = self.varianceofLap(imageGray)
+            if(i==0):
+                lt=v
+                i+=1
+                self.jogDown()
+                continue
+            else:
+                if(v>lt):
+                    lt=v
+                    self.jogDown()
+                    continue
+                else:
+                    if(loop>3):
+                        break
+                    else:
+                        loop += 1
+                        if(self.curPos<=self.minPos):
+                            self.jogUp()
+                            lt = v
+                            continue
+                        else:
+                            self.jogDown()
+                            lt = v
+                            continue
+
+
+
+
+
+
+
+
+
     def zVar(self):
         print("zVar")
         
