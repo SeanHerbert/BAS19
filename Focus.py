106eb6a4c18f07f7f6d2c393bcb4fb85f8139c24
Sean Herbert <seanh@email.arizona.edu>
2019-09-17 23:37:57 -0700

    simplified autofocus and put individual images in sepearte folders to simulate actual variance pattern during af

--- Focus.py
+++ Focus.py
@@ -4,90 +4,85 @@ import RPi.GPIO as GPIO
 import time
 from DRV8825 import DRV8825
 from imutils import paths
+from PIL import Image
+from os import walk
+
 
 class Focus():
     def __init__(self):
         self.motor = DRV8825(dir_pin=13, step_pin=19, enable_pin=12, mode_pins=(16, 17, 20))
         self.curPos = self.goToMax()
-        self.maxPos = 35
-        self.minPos = 25
     
-    def varianceofLap(image):
+    def varianceofLap(self,image):
         return cv2.Laplacian(image,cv2.CV_64F).var()
 
     def goToMax(self):
         #moveToMax pos
-        max = 35
-        return max
+        #for testing without limit switches, we assuming it takes 5 steps 
+        #the for loop will be replaced with the following while loop
+        #while(!self.isAtTop()):
+        for i in range(5):
+            self.motor.TurnStep(Dir='backward', steps=32, stepdelay = 0.000001) #probably need to take bigger steps
+        print("=====================set to Top===========================")
+        return 10000  #10mm from bottom limit switch
 
-    def vidOn(self):
-        print("vidOn")
-        
+    def isAtTop(self):
+        #poll top limit switch
+        return tls
     
-    
-    def vidOff(self):
-        print("vidOff")
-        
+    def isAtBottom(self):
+        #poll bottom limit switch
+        return bls
     
     def autoFocus(self):
-        # for imagePaths in paths.list_images('images'):
-        #     image = cv2.imread(imagePaths)
-        #     imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         loop = 0
         lt = 0
-        i=0
-
-        for imagePaths in paths.list_images('images'):
-            image = cv2.imread(imagePaths)
+        p=0 # used for indexing thru roche images (for testing only)
+    
+        while(1):
+            p+=1 # index thru folders of Roche images
+            for (dirpath, dirnames, ifn) in walk('/home/pi/BAS/Images/i'+str(p)):
+                f=ifn
+            image = cv2.imread('/home/pi/BAS/Images/i'+str(p)+'/'+str(f[0])) #for test: image grabbed from roche images(will be from camera)
             imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
             v = self.varianceofLap(imageGray)
-            if(i==0):
-                lt=v
-                i+=1
+            print("Variance at folder {} is {}".format(p,v))
+            if(loop ==3):
+                break
+            
+            if(v>lt):
+                lt =v
                 self.jogDown()
-                continue
+                
             else:
-                if(v>lt):
-                    lt=v
-                    self.jogDown()
-                    continue
-                else:
-                    if(loop>3):
-                        break
-                    else:
-                        loop += 1
-                        if(self.curPos<=self.minPos):
-                            self.jogUp()
-                            lt = v
-                            continue
-                        else:
-                            self.jogDown()
-                            lt = v
-                            continue
-
-
-
-
-
-
+                lt =v
+                self.jogUp()
+                p-=2 # used to simulate grabbing the previous image (one jog up)
+                loop +=1
+                
+                    
+                
 
 
 
     def zVar(self):
-        print("zVar")
+        return self.curPos
         
-    
-    
-    def setStop(self):
-        print("setStop")
-        
-    
     def jogUp(self):
+        #if(!selfisAtTop()):   //this will poll limit switch
         self.motor.TurnStep(Dir='backward', steps=32, stepdelay = 0.000001)
-        
-    
+        self.curPos +=0.5
+
     def jogDown(self):
+        #if(!isAtBottom()):   //this will poll limit switch
         self.motor.TurnStep(Dir='forward', steps=32, stepdelay = 0.000001)
+        self.curPos -=0.5
+    
+    def vidOn(self):
+        print("vidOn")
+        
+    def vidOff(self):
+        print("vidOff")
         
         
         
